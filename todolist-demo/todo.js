86ad34af8f690e6cdaa060ba8d63d49b9746247c
yizhengyuan <1711840294@qq.com>
2023-04-24 00:02:32 +0800

    Debug the checked symbol of each line and the delete button on initial items.

--- todolist-demo/todo.js
+++ todolist-demo/todo.js
@@ -1,7 +1,8 @@
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
+var myNodelist = document.getElementsByTagName("li");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
+
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
@@ -21,11 +22,12 @@ for (i = 0; i < close.length; i++) {
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+// list.addEventListener('click', function(ev) {
+//   if (ev.target.tagName === 'li') {
+//     ev.target.classList.toggle('checked');
+//   }
+// }, false);
+list.addEventListener('click', function(ev) {ev.target.classList.toggle('checked');}, false);
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
